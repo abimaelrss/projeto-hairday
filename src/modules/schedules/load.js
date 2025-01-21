d5691963640e35b1d18b9b5bfff9e6d414c58086
Abimael <abimaelrss@gmail.com>
2025-01-21 10:32:48 -0300

    Searching for schedules in the API

--- src/modules/schedules/load.js
+++ src/modules/schedules/load.js
@@ -1,11 +1,16 @@
+import { scheduleFetchByDay } from "../../services/schedule-fetch-by-day.js";
 import { hoursLoad } from "../form/hours-load.js";
 
 // Seleciona o input de data
 const selectedDate = document.getElementById("date");
 
-export function schedulesDay() {
+export async function schedulesDay() {
   // Obtém a data do input
   const date = selectedDate.value;
+
+  // Busca os agendamentos na API
+  const dailySchedules = await scheduleFetchByDay({ date });
+  console.log(dailySchedules)
 
   // Renderiza as horas disponíveis
   hoursLoad({ date });
--- src/services/schedule-fetch-by-day.js
+++ src/services/schedule-fetch-by-day.js
@@ -10,13 +10,13 @@ export async function scheduleFetchByDay({ date }) {
     const data = await response.json();
 
     // Filtra os agendamentos pelo dia selecionado
-    const dailySchedules = data.filter((schedule) => {
-      dayjs(date).isSame(schedule.when, "day");
-    });
+    const dailySchedules = data.filter((schedule) =>
+      dayjs(date).isSame(schedule.when, "day")
+    );
 
     return dailySchedules;
   } catch (error) {
     console.log(error);
-    alert("Não foi possível buscar os agendamentos do dia selecionado");
+    alert("Não foi possível buscar os agendamentos do dia selecionado!");
   }
 }
