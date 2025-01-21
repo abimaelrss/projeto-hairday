1f0fa046fe2fb402bf6ddd14973f3f551af58d46
Abimael <abimaelrss@gmail.com>
2025-01-21 19:32:33 -0300

    Blocking already scheduled times

--- src/modules/form/hours-load.js
+++ src/modules/form/hours-load.js
@@ -4,21 +4,27 @@ import { hoursClick } from "./hours-click";
 
 const hours = document.getElementById("hours");
 
-export function hoursLoad({ date }) {
+export function hoursLoad({ date, dailySchedules }) {
   // Limpa a lista de horários
   hours.innerHTML = "";
+
+  const unavailableHours = dailySchedules.map((schedule) =>
+    dayjs(schedule.when).format("HH:mm")
+  );
 
   const opening = openingHours.map((hour) => {
     // Recupera somente a hora
     const [schedulesHour] = hour.split(":");
 
     // Adiciona a hora na data e verifica se ela está no passado
-    const isHourPast = dayjs(date).add(schedulesHour, "hour").isAfter(dayjs());
+    const isHourPast = dayjs(date).add(schedulesHour, "hour").isBefore(dayjs());
+
+    const available = !unavailableHours.includes(hour) && !isHourPast;
 
     // Define se o horário está disponível
     return {
       hour,
-      available: isHourPast,
+      available,
     };
   });
 
